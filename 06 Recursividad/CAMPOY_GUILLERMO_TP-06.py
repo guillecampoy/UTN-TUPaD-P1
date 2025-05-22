b922843acac35099ed34e8af7e150dd433025bc7
Guillermo Campoy <guillecampoy@gmail.com>
2025-05-22 15:52:19 -0300

    Set up para re utilización de funciones para presentación Se crea una carpeta con archivos de soporte con funciones re utilizables Para este primer uso, a partir de ahora se comienza a utilizar de forma centralizada las funciones para dibujar encabezado, separador y pie de página Adicionalmente, se agrega carpeta para configuraciones de vscode

---
+++ 06 Recursividad/CAMPOY_GUILLERMO_TP-06.py
@@ -0,0 +1,2 @@
+# Se utiliza el archivo de presentación que contiene funciones básicas para imprimir encabezado, separadores y pie de página
+from Utils.Presentacion import print_header, print_separator, print_footer
